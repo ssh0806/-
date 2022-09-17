af2ee55c8c231e63957e99fa635c36fa285b8b3a
ssh0806 <112235871+ssh0806@users.noreply.github.com>
2022-09-17 15:20:00 +0900

    반복 심화

---
+++ 새 폴더/반복 문제.py
@@ -0,0 +1,39 @@
+# 고객 수 만큼 몸무게와 키를 입력 받으시오
+"""number=int(input("인원수:"))
+for i in range(number):
+    키,몸무게=input("키: , 몸무게:").split() # 170 56
+    키=int(키) 
+    몸무게=int(몸무게)
+    표준체중=(키-100)*0.9
+    비만기준체중=표준체중*1.2
+    if 몸무게 >비만기준체중:
+        print("비만")
+    else:
+        print("비만 아님")"""
+
+"""a=["o","x","o","o","o","x","x"]
+n=0
+m=0
+for i in a:
+    if i =="o":
+        n+=1
+    elif i=="x":
+        m+=1
+print(n,m)"""
+
+b=["S","M","L","S","L","L"]
+q=0
+w=0
+e=0
+for i in b:
+    if i =="S":
+        q+=1
+    elif i=="M":
+        w+=1
+    elif i=="L":
+        e+=1
+print(q,w,e,)
+
+    
+
+    
---
+++ 새 폴더/반복문 심화.py
@@ -0,0 +1,24 @@
+"""for i in range(1,11):
+   print(i)""" #1부터 10까지 출력
+#1부터 10 다 더하기
+"""s=0
+for i in range(1,11):
+    s+=i 
+
+print(s)"""
+#짝수만 구하기
+"""for i in range (1,11):
+    if i%2 == 0:
+        s+=i
+print(s)"""
+
+"""a=int(input())
+for i in range (1,a+1):
+    print(i)"""
+
+a=int(input())
+s=0
+for i in range(a):
+    n=int(input())
+    s+=n
+print(s)
